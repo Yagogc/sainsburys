2d1bb736d5c8cce701911c9f4f1cea4950543c1b
Yago <jagogc@gmail.com>
2018-04-28 19:22:04 +0100

    Fetch from Flickr and font added.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import fetchJsonp from "fetch-jsonp";
 
 class App extends Component {
+  state = {
+    items: ""
+  };
+  componentDidMount() {
+    const self = this;
+    fetchJsonp(
+      "//api.flickr.com/services/feeds/photos_public.gne?format=json",
+      {
+        jsonpCallbackFunction: "jsonFlickrFeed"
+      }
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        console.log(json.items);
+        self.setState({
+          items: json.items
+        });
+      })
+      .catch(function(ex) {
+        console.log("parsing failed", ex);
+      });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Sainsbury</h1>
         </header>
-        <p className="App-intro">
+        <main className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </main>
       </div>
     );
   }
